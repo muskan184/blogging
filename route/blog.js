b4808271b4c98d21e254d86c5c1f4d4f754ab411
muskan dubey <muskandubey184@gmail.com>
2024-12-24 23:07:49 +0530

    blogging site

--- route/blog.js
+++ route/blog.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const Blog = require("../model/blog");
 const multer = require("multer");
 const path = require("path");
+const Comment = require("../model/comment");
 
 const router = Router();
 
@@ -23,13 +24,33 @@ router.get("/add-new", (req, res) => {
   });
 });
 
+router.get("/:id", async (req, res) => {
+  const blog = await Blog.findById(req.params.id).populate("createdBy");
+  const comments = await Comment.find({}).populate("createdBy");
+  console.log("comment", comments);
+  return res.render("blog", {
+    user: req.user,
+    blog,
+    comments,
+  });
+});
+
+router.post("/comment/:blogId", async (req, res) => {
+  await Comment.create({
+    content: req.body.content,
+    blogId: req.body.blogId,
+    createdBy: req.user._id,
+  });
+  return res.redirect(`/blog/${req.params.blogId}`);
+});
+
 router.post("/", upload.single("coverimage"), async (req, res) => {
   const { title, body } = req.body;
   const blog = await Blog.create({
     title,
     body,
     createdBy: req.user._id,
-    coverImageURl: `uploads/${req.file.filename}`,
+    coverImageURl: `/${req.file.filename}`,
   });
   return res.redirect("/");
 });
